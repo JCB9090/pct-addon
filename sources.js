802771edb510f53f5ade7d4f2f955d3698bb261f
JCB9090 <jbc9090@tuta.io>
2015-12-11 10:54:24 +0000

    Create sources.js

---
+++ sources.js
@@ -0,0 +1,4 @@
+module.exports = { 
+  eztv: ["https://popcorntime.ws/api/eztv", "https://popcornwvnbg7jev.onion.to"],
+  yts: ["https://yts.ag" /*, "http://api.torrentsapi.com"*/ ] // torrents api might not be compat
+}
