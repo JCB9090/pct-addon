ef0a0f87918ef09e23dc7665ec72e06560239bba
jcb9090 <jbc9090@tuta.io>
2015-12-12 11:28:20 -0500

    completed eztv crawl, cache save to diks

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var _ = require("lodash");
 var async = require("async");
 var needle = require("needle");
 var fs = require('fs');
+var magnet = require('magnet-uri');
+var path = require('path');
 
 var manifest = { 
     "name": "Popcorn",
@@ -16,21 +18,31 @@ var manifest = {
 
 /* SERVE DATA
  */
+var cachePath = path.join(process.env.HOME || require("os").tmpdir(), "popcorn-cache.json");
 var map = { };
-//var cacheMap = function() { }
+try { map = JSON.parse(fs.readFileSync(cachePath).toString()) } catch(e) { console.error("non-fatal( cache)", e) }
+console.log("-> map has "+Object.keys(map).length+" movies / eps");
+
+setInterval(function() {
+    var start = Date.now();
+    fs.writeFile(cachePath, JSON.stringify(map), function(e) { if (e) console.error(e) });
+    console.log("-> stringifying cache took "+(Date.now()-start)+"ms");
+}, 20*1000);
 
 var addon = new stremio.Server({
     "stream.find": function(args, callback, user) {
         if (! args.query) return callback();
 
-        var hash = (args.query.hasOwnProperty('season') ? [args.query.imdb_id, args.query.season, args.query.episode] : [args.query.imdb_id]).join(" ");
-        //console.log(hash)
+        var isEp = args.query.hasOwnProperty('season');
+        var hash = (isEp ? [args.query.imdb_id, args.query.season, args.query.episode] : [args.query.imdb_id]).join(" ");
 
         callback(null, _.map(map[hash] || [], function(infoHash, quality) { 
             return {
-                infoHash: infoHash,
-                tag: [quality].concat(quality == "1080p" ? ["hd"] : []),
-                name: args.query.hasOwnProperty('season') ? "EZTV" : "YTS"
+                infoHash: infoHash.toLowerCase(),
+                tag: [quality].concat(quality == "1080p" ? ["hd"] : []).concat(isEp ? "eztv" : "yts"),
+                name: isEp ? "EZTV" : "YTS",
+                sources: ['tracker:udp://tracker.leechers-paradise.org:6969/announce', 'tracker:udp://tracker.pomf.se:80/announce', 'tracker:http://tracker.aletorrenty.pl:2710/announce'],
+                availability: 2 // todo: from seed/leech count
             }
         }));
     }
@@ -47,7 +59,10 @@ var server = require("http").createServer(function (req, res) {
  */
 var httpOpts = { 
     headers: { user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/28.0.1500.52 Chrome/28.0.1500.52 Safari/537.36" },
-    json: true
+    json: true,
+    open_timeout: 4000,
+    timeout: 4000,
+    read_timeout: 4000
 };
 var queue = async.queue(function(url, next) {
     console.log("-> collecting from "+url);
@@ -56,9 +71,17 @@ var queue = async.queue(function(url, next) {
         if (err) console.error(err);
 
         // eztv - initial - list of /shows/N
-        if (Array.isArray(body) && body[0] && body[0].match("shows")) body.forEach(function(page) {
+        if (Array.isArray(body) && body[0] && typeof(body[0])=="string" && body[0].match("shows")) body.forEach(function(page) {
             queue.push(url.replace('/shows/', '/'+page));
         });
+
+        // eztv - shows listing
+        if (Array.isArray(body) && body[0] && body[0].tvdb_id) body.reverse().forEach(function(show) {
+            queue.push(url.split('/shows')[0]+'/show/'+show._id);
+        });
+
+        // eztv - show
+        if (body._id && body.imdb_id && body.tvdb_id) indexShow(body);
 
         // yts api - initial - /v2/list_movies.json
         if (body.status && body.data && body.data.movies) { 
@@ -68,19 +91,34 @@ var queue = async.queue(function(url, next) {
             if (body.data.page_number * body.data.limit < body.data.movie_count) 
                 queue.push(url.split("?")[0]+"?page="+(body.data.page_number+1));
         }
-
-        console.log(url, body)
     });
 }, 1);
 
 var sources = require("./sources");
 sources.yts.forEach(function(url) { queue.push(url) });
-//sources.eztv.forEach(function(url) { queue.push(url) });
+sources.eztv.forEach(function(url) { queue.push(url) });
 
 function indexMovie(movie) {
-    //map[]
+    if(movie && Array.isArray(movie.torrents)) movie.torrents.forEach(function(t) {
+        if (!map[movie.imdb_code]) map[movie.imdb_code] = { };
+        map[movie.imdb_code][t.quality] = t.hash; // todo: other stuff
+    });
 }
 
 function indexShow(show) {
+    if (! (show && show.imdb_id && show.episodes && Array.isArray(show.episodes))) return;
+    var imdb_id = show.imdb_id;
+    
+    show.episodes.forEach(function(ep) {
+        var hash = imdb_id+" "+ep.season+" "+ep.episode;
+        if (!map[hash]) map[hash] = { };
 
+        _.each(ep.torrents, function(tor, quali) { 
+            try {
+                map[hash][quali] = magnet.decode(tor.url).infoHash
+            } catch(e) { console.error('magnet.decode', e) }
+        });
+        var m = map[hash];
+        if (m['0'] && (m['1080p'] == m['0'] || m['720p'] == m['0'] || m['480p'] == m['0'])) delete m['0'];
+    });
 }
--- sources.js
+++ sources.js
@@ -1,4 +1,7 @@
 module.exports = { 
-  eztv: ["https://popcorntime.ws/api/eztv", "https://popcornwvnbg7jev.onion.to"],
-  yts: ["https://yts.ag" /*, "http://api.torrentsapi.com"*/ ] // torrents api might not be compat
+  // must point to /shows/ endpoint
+  eztv: ["https://popcorntime.ws/api/eztv/shows/", "https://popcornwvnbg7jev.onion.to/shows/"],
+
+  // must point to /api/v2/list_movies.json
+  yts: ["https://yts.ag/api/v2/list_movies.json" /*, "http://api.torrentsapi.com/api/v2/list_movies.json"*/ ] // torrents api might not be compat
 }
